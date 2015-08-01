5c07981b9030fb41d0f399c0a9bf0fe46b69424a
Ehsan Rafeeque <ehsan65505@gmail.com>
2015-08-01 09:19:10 -0400

    add fullName function in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
  
   has_many :statuses
+
+
+  def fullName
+  	first_name+" "+last_name
+  end
+
 end
