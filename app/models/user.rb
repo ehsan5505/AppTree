fc430ca0de7e669d72ce70f0d200ebe09bf03709
Ehsan Rafeeque <ehsan65505@gmail.com>
2015-08-02 01:42:32 -0400

    remove format from username

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,10 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true
-  validates :username, uniqueness: true 
+  validates :username, uniqueness: true
+   # format= {
+  	# 		with: /^[A-Za-z1-9]/,
+  	# 		message: "Please provide correct format for username"}; 
 
   def fullName
   	first_name+" "+last_name
