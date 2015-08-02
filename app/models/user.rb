551df388491a72328a7825d87af16e13a22bd826
Ehsan Rafeeque <ehsan65505@gmail.com>
2015-08-02 01:28:30 -0400

    Add username format and message

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ActiveRecord::Base
  
   has_many :statuses
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, format: {
+  								with: /^[A-Za-z1-9]/,
+  								message : "Please correct Username format"
+  								} 
 
   def fullName
   	first_name+" "+last_name
